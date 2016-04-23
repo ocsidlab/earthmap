b1b227b3cceae86b8107bae19b6dc4c1c5405500
Arun Ganesh <arun.planemad@gmail.com>
2016-04-24 01:02:58 +0530

    Update style

--- js/map.js
+++ js/map.js
@@ -2,7 +2,7 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoicnVjaGlrYW5hbWJpYXIiLCJhIjoiY2ltZWpiMXNpMDB6OXUxa2swN3VqbDEyZyJ9.eBbIitymKP-QVel_NGj_Cw';
 var map = new mapboxgl.Map({
   container: 'map', // container id
-  style: 'mapbox://styles/mapbox/streets-v8', //stylesheet location
+  style: 'mapbox://styles/touchstoneist/cindi7tv50100cxnhxul9uwtf', //stylesheet location
   hash: true,
   zoom: 4,
   center: [79.0806091, 21.1498041]
@@ -14,17 +14,38 @@ var map = new mapboxgl.Map({
 var airQualityRaw;
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function() {
+
     if (xhr.readyState == XMLHttpRequest.DONE) {
-        airQualityRaw = xhr.responseText;
-        console.log(airQualityRaw);
+
+        airQuality = JSON.parse(xhr.responseText);
+        GeoJSON.parse(airQuality,{Point: ['lat', 'lon']});
+        console.log(airQuality);
+
+        // Create dynamic layer
+        map.on('style.load', function(e) {
+
+           var sourceObj = new mapboxgl.GeoJSONSource({
+             data: airQuality,
+             buffer: 0
+           });
+
+           map.addSource('aqi', sourceObj);
+
+           map.addLayer({
+        "id": "aqi-all",
+        "type": "symbol",
+        "source": "aqi",
+        "layout": {
+            "icon-image": "{marker-symbol}-15",
+            "text-field": "{name}",
+            "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+            "text-offset": [0, 0.6],
+            "text-anchor": "top"
+        }
+    });
+
+        });
     }
 }
-xhr.open('GET', 'http://aqi.indiaspend.org/aq/api/aqfeed/latestAll/?format=json', true);
+xhr.open('GET', 'http://aqi.indiaspend.org/aq/api/aqfeed/latestAll/?format=json ', true);
 xhr.send(null);
-
-// Dynamic map styling
-map.on('style.load', function(e) {
-
-
-
-});
